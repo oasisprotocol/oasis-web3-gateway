0d11401e399e7c975d5f458923f6971c86f6adb0
simon <hesynote@gmail.com>
2021-11-03 15:29:00 +0800

    rpc: implement method web3_clientVersion

--- rpc/web3/api.go
+++ rpc/web3/api.go
@@ -1,8 +1,12 @@
 package web3
 
 import (
+	"fmt"
+	"runtime"
+
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/starfishlabs/oasis-evm-web3-gateway/version"
 )
 
 // PublicAPI is the web3_ prefixed set of APIs in the Web3 JSON-RPC spec.
@@ -16,4 +20,9 @@ func NewPublicAPI() *PublicAPI {
 // Sha3 returns the keccak-256 hash of the passed-in input.
 func (a *PublicAPI) Sha3(input hexutil.Bytes) hexutil.Bytes {
 	return crypto.Keccak256(input)
+}
+
+// ClientVersion returns the current client info.
+func (a *PublicAPI) ClientVersion() string {
+	return fmt.Sprintf("oasis/%s/%s", version.Version(), runtime.Version())
 }
