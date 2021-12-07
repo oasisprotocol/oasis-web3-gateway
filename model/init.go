b79f6c5eeeeac35961345d989a2aa858bb3f65a6
Kevin <chensiqi_scdy@163.com>
2021-12-07 02:40:15 -0800

    fix linter.

--- model/init.go
+++ model/init.go
@@ -5,24 +5,32 @@ import (
 	"github.com/uptrace/bun"
 )
 
+var tables = []interface{}{
+	new(Block),
+	new(BlockRef),
+	new(Transaction),
+	new(TransactionRef),
+	new(IndexedRoundWithTip),
+	new(Receipt),
+	new(Log),
+}
+
 // CreateTables creates tables.
 func CreateTables(db *bun.DB) error {
-	var err error
-	// tables
-	tables := []interface{}{
-		new(Block),
-		new(BlockRef),
-		new(Transaction),
-		new(TransactionRef),
-		new(IndexedRoundWithTip),
-		new(Receipt),
-		new(Log),
+	for _, tb := range tables {
+		_, err := db.NewCreateTable().Model(tb).IfNotExists().Exec(context.Background())
+		if err != nil {
+			return err
+		}
 	}
 
-	// create tables
-	for _, tb := range tables {
-		_, err = db.NewCreateTable().Model(tb).IfNotExists().Exec(context.Background())
-		if err != nil {
+	return nil
+}
+
+// TruncateModel clears any DB records.
+func TruncateModel(db *bun.DB) error {
+	for m := range tables {
+		if _, err := db.NewDropTable().Model(m).IfExists().Exec(context.Background()); err != nil {
 			return err
 		}
 	}
